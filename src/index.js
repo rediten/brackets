b3e46e6bc56e43d99bdf7b94da25577227c69596
rediten <107889485+rediten@users.noreply.github.com>
2022-07-27 21:17:55 +0300

    feat: implement function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  str = str.split('');
+  
+  if (str.length % 2 !== 0) return false;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] !== bracketsConfig[j][1]) { 
+        if (str[i] === bracketsConfig[j][0]) {
+          stack.push(str[i]);
+        } else if (str[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0] && stack.length !== 0) {
+            stack.pop()
+          } 
+        } else if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+          if (stack.length === 0 && str[i] === bracketsConfig[j][0]) {
+            stack.push(str[i]);   
+          } else if (str[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0] && stack.length !== 0) {
+            stack.pop() 
+            } 
+       }
+    }
+  }
+  return (stack.length === 0) ? true : false;     
 }
